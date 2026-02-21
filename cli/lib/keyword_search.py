8a88ad3be72ac6e052223a7afa5c1a2843e7501d
Karim OCB <chaya.karim.omar@gmail.com>
2026-02-21 16:45:54 +0000

    AC: Keyword Search Doctor Info

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,9 +1,16 @@
 from lib.search_utils import load_doctors
 
-def search_command(query):
+def search_command(query: str):
     doctors_data = load_doctors()
+    processed_query = text_processing(query)
     result_names = []
-    for _, doctor in enumerate(doctors_data):
-        if query in doctor["name"]:
+    for _, doctor in enumerate(doctors_data): # {"id": "DR208", "name": "Dr. Frida Kahlo", "age": 42, "specialty": "Pain Rehabilitation", "availability": "Mon-Wed 11:00-16:00", "bio": "Expert in managing..."},
+        doctor_info = f"Name: {doctor["name"]}, age: {doctor["age"]}, specialty: {doctor["specialty"]}, bio: {doctor["bio"]}, availability: {doctor["availability"]}" 
+        processed_doctor_info = text_processing(doctor_info)
+        if query in processed_doctor_info:
             result_names.append(doctor["name"])
     return result_names
+
+def text_processing(text: str):
+    lowercase_text = text.lower()
+    return lowercase_text
