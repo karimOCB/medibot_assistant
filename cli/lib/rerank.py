b18e49eaf4967e887ade201c9f6dec0968e75b7d
Karim OCB <chaya.karim.omar@gmail.com>
2026-03-07 21:34:50 +0000

    BE: Batch LLM Re-Ranking

--- cli/lib/rerank.py
+++ cli/lib/rerank.py
@@ -1,4 +1,4 @@
-import os, time
+import os, time, json
 from dotenv import load_dotenv
 from google import genai
 
@@ -35,9 +35,49 @@ def individual_rerank(results: list[dict], query: str) -> list[dict]:
     
     return sorted(results, key=lambda item: item["rerank_score"], reverse=True)
 
-def rerank_results(results: list[dict], query: str, method: str) -> None:
+
+def batch_rerank(results: list[dict], query: str) -> list[dict]:
+        drs_list_info = []
+        drs_dict = {}
+        for result in results:
+            drs_list_info.append(f'{result['doc']['id']} - {result['doc']['name']} - {result['doc']['specialty']} - {result['doc']['bio']} - {result['doc']['age']} - {result['doc']['availability']}')
+            drs_dict[result["doc"]["id"]] = result["doc"]
+        prompt = f"""
+            Rank the medical providers/doctors listed below by their relevance to the specific patient inquiry.
+
+            Query: "{query}"
+
+            Doctors Information:
+            {drs_list_info}
+
+            Consider:
+            - Precise specialty alignment (e.g., matching 'Heart' to 'Cardiology' over 'General Surgery').
+            - Specificity of the clinical expertise mentioned in the doctor's bio.
+            - Alignment with requested availability or specific hospital departments.
+            - Mention of specific procedures or symptoms requested by the user.
+
+            Return ONLY the Provider IDs (or indices) in order of relevance (best match first). 
+            Return a valid JSON list of integers/IDs, nothing else.
+
+            For example:
+            ["DR502", "DR108", "DR334", "DR21", "DR9"]
+
+            Ranking:"""
+        response = client.models.generate_content(
+        model='gemma-3-27b-it', contents=prompt)
+        ranking = json.loads(response.text)
+        id_to_rank = {id: i for i, id in enumerate(ranking)}
+        results = sorted(results, key=lambda result: id_to_rank[result["doc"]["id"]])
+        print(ranking)
+        print(results)
+        return results
+
+def rerank_results(results: list[dict], query: str, method: str) -> list[dict]:
     match method:
         case "individual":
             return individual_rerank(results, query)
+        case "batch":
+            return batch_rerank(results, query)
         case _:
             return results
+        
